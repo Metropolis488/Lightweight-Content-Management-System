77daee65bb5539f688a529a5d76001a077a22519
Matt Steiner <Metropolis.4488@gmail.com>
2020-11-09 19:55:02 -0500

    added SQL files:

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE blog_db;
+
+INSERT INTO blogPost (post, title, author, abstract) VALUES ("THis is a test of the national broadcast system.  Please turn off your tv and walk outside.", "This is a TEST", "Slippin' Jimmy McGill", "An abstract to walking outside instead of looking at the idot box.");
+INSERT INTO blogPost (post, title, author, abstract) VALUES ("This is some more stuff.", "All about cooking", "Gordon Ramsey", "Season your food - DAMN!");
+INSERT INTO blogPost (post, title, author, abstract) VALUES ("Cook with Butter", "A note from Julia Child", "Julia Child", "Everything is better with butter");
+INSERT INTO blogPost (post, title, author, abstract) VALUES ("asdfkasf;lask asdf asdf sadf asdf asdf asd fas asd.", "Dumb stuff", "This guy", "Season your food.");
